6b0264a54b4bcd6cde0fc2d98c7179a255fbe205
Michael Hohlios <mhohlios@sagesocial.com>
2024-05-30 11:47:32 -0400

    testing out explicit import

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in {
-        legacyPackages = import ./. { inherit system; };
+        legacyPackages = import ./node-packages.nix { inherit system; };
       }
     );
 }
