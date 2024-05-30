3fa78a6581495df55ea232143746c2c6aee69f89
Michael Hohlios <mhohlios@sagesocial.com>
2024-05-30 11:14:32 -0400

    adding flake.nix for flake importing

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Sage specific NodePackages for Nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        legacyPackages = import ./. { inherit system; };
+      }
+    );
+}
