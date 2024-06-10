69b8c945a06fd76d987bcfb9b9d637f1ff4e856b
Michael Hohlios <mhohlios@sagesocial.com>
2024-06-10 13:04:00 -0400

    Updating flake.nix to work as a shell and hopefully with packages and updating the node2nix files

--- flake.nix
+++ flake.nix
@@ -2,16 +2,24 @@
   description = "Sage specific NodePackages for Nix";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    };
+  };
 
-  outputs = { self, flake-utils, nixpkgs }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        legacyPackages = import ./node-packages.nix { inherit system; };
-      }
+        let
+            pkgs = import nixpkgs { inherit system; };
+            nodeEnv = pkgs.callPackage ./composition.nix {};
+        in
+        {
+            packages.default = nodeEnv.nodeDependencies;
+            devShells.default = pkgs.mkShell {
+                buildInputs = [ pkgs.nodejs_20 nodeEnv.nodeDependencies ];
+                shellHook = ''
+                echo "Welcome to the development environment for sage-node-pkgs"
+                '';
+            };
+        }
     );
 }
